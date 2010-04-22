088187b68cd8d5212a2932147d814618566c811d
Amitaibu <amitai@gizra.com>
2010-04-22 20:21:15 +0300

    Include zen settings.

--- template.php
+++ template.php
@@ -1,2 +1,9 @@
 <?php
 // $Id: template.php,v 1.4 2009/12/08 02:00:26 duvien Exp $
+
+// Include the definition of zen_theme_get_default_settings().
+include_once drupal_get_path('theme', 'zen_starter') . '/theme-settings.php';
+
+function zen_ninesixty_settings() {
+
+}
